76b9b314f4dbdf6b953fb513da83bd1b3d263a86
GMcD <gary.macdonald@projectscapa.com>
2016-09-19 22:19:24 +0100

    Django 1.9.

--- embed9/utils.py
+++ embed9/utils.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 from django.core.exceptions import ImproperlyConfigured
-from django.db.models import get_app, get_models
+# from django.db.models import get_app, get_models
+from django.apps import apps
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 
@@ -18,10 +19,10 @@ def get_appmodel(app_name, model_name):
     app_models = APPS_MODELS.get(app_name)
     if not app_models:
         try:
-            app = get_app(app_name)
+            app = apps.get_app(app_name)
         except ImproperlyConfigured:
             raise Http404
-        app_models = APPS_MODELS[app_name] = dict((model._meta.model_name, model) for model in get_models(app))
+        app_models = APPS_MODELS[app_name] = dict((model._meta.model_name, model) for model in apps.get_models(app))
         
     model = app_models.get(model_name)
     if not model:
